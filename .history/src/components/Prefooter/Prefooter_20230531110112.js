cc4ff4e8f8e4290fdae8c6faa0cc79e60da4ceea
Sarah <sarahnnanyelugo@gmail.com>
2023-05-31 11:48:56 +0100

    tour component and prefooter

--- .history/src/components/Prefooter/Prefooter_20230531110112.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "/prefooter.scss";
-function Prefooter() {
-  return <div className="col-md-12 prefooter-container">Prefooter</div>;
-}
-
-export default Prefooter;
--- .history/src/components/Tour/Tour_20230531113613.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Tour() {
-  return <div className="col-md-12 flexy tour-container"></div>;
-}
-
-export default Tour;
