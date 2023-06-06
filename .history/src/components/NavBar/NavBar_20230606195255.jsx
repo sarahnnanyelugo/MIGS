f8e54e9e1b16b2c586f0f54fb76e60612cb2234e
Sarah <sarahnnanyelugo@gmail.com>
2023-06-06 21:26:37 +0100

    landing page

---
+++ .history/src/components/NavBar/NavBar_20230606195255.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState, useRef } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./_navbar.scss";
+import { useLocation } from "react-router-dom";
+// import Icofont from "react-icofont";
+import Logo from "../../assets/images/logo2.png";
+
+function NavBar() {
+  return (
+    <>
+      <div className="nav-container col-md-10 offset-md-1">
+        {" "}
+        {/* <div className=" col-md-1 tech-logo-div">
+          {" "}
+          <NavLink to={"/"}>
+            <img className="tech-logo" src={Logo} alt="Scholar" width="60%" />
+          </NavLink>
+        </div> */}
+        <ul className=" list-inline list-unstyled">
+          {/* <li className="list-inline-item">
+            <NavLink className="tech-links cool-link" to={"/about-us"}>
+              About
+            </NavLink>
+          </li>
+          <li className="list-inline-item">
+            <NavLink className="tech-links cool-link" to={"/admission"}>
+              Admission
+            </NavLink>
+          </li>
+          <li className="list-inline-item">
+            <NavLink className="tech-links cool-link" to={"/eyfs"}>
+              Academics
+            </NavLink>
+          </li>
+          <li className="list-inline-item">
+            <NavLink className="tech-links cool-link" to={"/iec"}>
+              IEC
+            </NavLink>
+          </li>{" "}
+          <li className="list-inline-item">
+            <NavLink className="tech-links cool-link">E-shop</NavLink>
+          </li>
+          <li className="list-inline-item">
+            <NavLink className="tech-links cool-link" to={"/gallery"}>
+              Gallery
+            </NavLink>
+          </li>
+          <li className="list-inline-item">
+            <button className="nav-btn">e-portal</button>
+          </li>*/}
+          <li>
+            <div className="mobile-menu">
+              <div
+                className="dots"
+                onclick="this.classNameList.toggle('active');"
+              >
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="shadow cut"></div>
+                <div className="container cut">
+                  <div className="drop cut2"></div>
+                </div>
+                <div className="list">
+                  <ul>
+                    <li>Mark as read</li>
+                    <li>Flag as important</li>
+                    <li>Save for later</li>
+                    <li>Move to trash</li>
+                    <li>Mark as spam</li>
+                  </ul>
+                </div>
+                <div className="dot"></div>
+              </div>
+              <div
+                className="cursor"
+                onclick="document.querySelector('.dots').classNameList.toggle('active');"
+              ></div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
